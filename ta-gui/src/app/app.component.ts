4ff35d75c28f041792f9abffc6e6a532ce6b662b
luan1221 <lssa@cin.ufpe.br>
2021-11-06 11:59:51 -0300

    Elementos do formulario

--- ta-gui/src/app/app.component.ts
+++ ta-gui/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgModule } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+<<<<<<< HEAD
   title = 'Título do meu site';
+=======
+  aluno: Aluno = {nome: "", cpf: "", email: ""};
+}
+
+export class Aluno {
+  nome: string;
+  cpf: string;
+  email: string;
+>>>>>>> faf23c0 (elementos do formulario para cadastro de alunos)
 }
