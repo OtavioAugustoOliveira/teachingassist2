2f99927687aa369fae5c05b356dce50327aa4e00
unknown <otavioaugustomachadodeoliveira@gmail.com>
2022-04-04 16:56:29 -0300

    adicionando github

--- ta-gui/src/app/app.component.ts
+++ ta-gui/src/app/app.component.ts
@@ -7,11 +7,14 @@ import { NgModule } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  aluno: Aluno = {nome: "", cpf: "", email: ""};
+
+  aluno: Aluno = {nome: "", cpf: "", email: "", github: ""};
+  title: any;
 }
 
 export class Aluno {
   nome: string;
   cpf: string;
   email: string;
+  github: string;
 }
